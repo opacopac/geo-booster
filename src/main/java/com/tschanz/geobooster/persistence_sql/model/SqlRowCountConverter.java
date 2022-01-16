e75078df182ef51b12c1b6a865719bb6fa5f04c1
opacopac <armand@tschanz.com>
2022-01-16 11:44:34 +0100

    refactoring sql reader

--- src/main/java/com/tschanz/geobooster/persistence_sql/model/SqlRowCountConverter.java
+++ src/main/java/com/tschanz/geobooster/persistence_sql/model/SqlRowCountConverter.java
@@ -8,7 +8,7 @@ import java.sql.ResultSet;
 
 @RequiredArgsConstructor
 public class SqlRowCountConverter implements SqlGenericResultsetConverter<Long> {
-    private final static String COL_COUNT = "TKV_COUNT";
+    private final static String COL_COUNT = "ROW_COUNT";
 
     private final String table;
 
