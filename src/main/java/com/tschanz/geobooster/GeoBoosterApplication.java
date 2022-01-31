4ac76d26a0b270306afba2ebced0d759f3f7cc6c
opacopac <armand@tschanz.com>
2022-01-31 21:16:03 +0100

    fix: change default port, add readme

--- src/main/java/com/tschanz/geobooster/GeoBoosterApplication.java
+++ src/main/java/com/tschanz/geobooster/GeoBoosterApplication.java
@@ -3,9 +3,10 @@ package com.tschanz.geobooster;
 import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GeoBoosterApplication {
     @SneakyThrows
     public static void main(String[] args) {
