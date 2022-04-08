2f3aadc8079b4dcad39c490f280e90aff227944b
ue55617 <armand.tschanz@sbb.ch>
2022-04-08 10:22:26 +0200

    server mode: reload dr page

--- src/main/java/com/tschanz/geobooster/presentation_web/controller/WebController.java
+++ src/main/java/com/tschanz/geobooster/presentation_web/controller/WebController.java
@@ -1,5 +1,6 @@
 package com.tschanz.geobooster.presentation_web.controller;
 
+import com.tschanz.geobooster.presentation.actions.GbActions;
 import com.tschanz.geobooster.presentation.model.GbState;
 import com.tschanz.geobooster.presentation_web.model.CachingStatsData;
 import com.tschanz.geobooster.presentation_web.model.ConnectionData;
@@ -15,23 +16,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 public class WebController {
     private final GbState gbState;
+    private final GbActions gbActions;
 
 
     @GetMapping("/geo/web")
-    public String adminPage(Model model) {
+    public String showStatsPage(Model model) {
         var connectionData = new ConnectionData(this.gbState.getConnectionState());
         var cachingStatsData = new CachingStatsData(
-            this.gbState.getNetzRepoState().getBetreiberRepoState(),
-            this.gbState.getNetzRepoState().getVerwaltungRepoState(),
-            this.gbState.getNetzRepoState().getHaltestelleRepoState(),
-            this.gbState.getNetzRepoState().getVerkehrskanteRepoState(),
-            this.gbState.getNetzRepoState().getTarifkanteRepoState(),
-            this.gbState.getRtmRepoState().getHstWegangabeRepoState(),
-            this.gbState.getRtmRepoState().getRgKorridorRepoState(),
-            this.gbState.getRtmRepoState().getRgAuspraegungRepoState(),
-            this.gbState.getZonenRepoState().getZoneRepoState(),
-            this.gbState.getZonenRepoState().getZonenplanRepoState(),
-            this.gbState.getTarifRepoState().getAwbRepoState()
+                this.gbState.getNetzRepoState().getBetreiberRepoState(),
+                this.gbState.getNetzRepoState().getVerwaltungRepoState(),
+                this.gbState.getNetzRepoState().getHaltestelleRepoState(),
+                this.gbState.getNetzRepoState().getVerkehrskanteRepoState(),
+                this.gbState.getNetzRepoState().getTarifkanteRepoState(),
+                this.gbState.getRtmRepoState().getHstWegangabeRepoState(),
+                this.gbState.getRtmRepoState().getRgKorridorRepoState(),
+                this.gbState.getRtmRepoState().getRgAuspraegungRepoState(),
+                this.gbState.getZonenRepoState().getZoneRepoState(),
+                this.gbState.getZonenRepoState().getZonenplanRepoState(),
+                this.gbState.getTarifRepoState().getAwbRepoState()
         );
         var servingStatsData = new ServingStatsData(this.gbState.getWmsState());
         var progressData = new ProgressData(this.gbState.getProgressState());
@@ -41,6 +43,14 @@ public class WebController {
         model.addAttribute("servingStatsData", servingStatsData);
         model.addAttribute("progressData", progressData);
 
-        return "admin.html";
+        return "stats.html";
+    }
+
+
+    @GetMapping("/geo/reload")
+    public String showReloadDrPage(Model model) {
+        this.gbActions.getConnectionActions().loadDr();
+
+        return "reload_dr.html";
     }
 }
